5dd34ac0347baa99955ca0bdfd033c9c60839c08
Yannick <yannick@bestworldsweb.com>
2023-11-15 19:35:45 -0300

    Setting permissions to git hooks file sync

--- .warp/bin/git.sh
+++ .warp/bin/git.sh
@@ -11,11 +11,19 @@ function git_rsync()
         exit
     fi;
 
+    if [ $(warp_check_is_running) = false ]; then
+        warp_message_error "The containers are not running"
+        warp_message_error "please, first run warp start"
+
+        exit 0;
+    fi
+
     GIT_CONFIG_DIR="$CONFIGFOLDER/git"
 
     if [ -d "$GIT_CONFIG_DIR" ]
     then
-      rsync -azvP "$GIT_CONFIG_DIR/" "$PROJECTPATH/.git/" > /dev/null
+        rsync -azvP "$GIT_CONFIG_DIR/" "$PROJECTPATH/.git/" > /dev/null
+        chmod -R 755 "$PROJECTPATH/.git/hooks"
     fi
 
 }
