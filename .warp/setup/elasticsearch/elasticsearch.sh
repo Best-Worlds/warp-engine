cf7bfc97d5d8573bdcd22e07810b9158477c68d3
yannickbw <yannick@bestworlds.com>
2024-08-12 18:20:16 -0300

    NO_TKT: Adding ElasticSearch service to new approach

--- .warp/setup/elasticsearch/elasticsearch.sh
+++ .warp/setup/elasticsearch/elasticsearch.sh
@@ -14,20 +14,20 @@ done
 
 if [ "$respuesta_es" = "Y" ] || [ "$respuesta_es" = "y" ]
 then
-    warp_message_info2 "You can check the available versions of elasticsearch here $(warp_message_info '[ https://hub.docker.com/r/devbestworlds/elasticsearch/tags/ ]')"
-    
+    warp_message_info2 "You can check the available versions of elasticsearch here $(warp_message_info '[ '$(get_docker_image_repository_url "elasticsearch")' ]')"
+
+    image_tags_switch=$(get_docker_image_tags_switch 'elasticsearch')
+    image_tags=$(get_docker_image_tags 'elasticsearch')
+    last_tag=$(get_docker_image_last_tag 'elasticsearch')
     while : ; do
-        elasticsearch_version=$( warp_question_ask_default "Choose a version of elasticsearch: $(warp_message_info [5.6.8]) " "5.6.8" )
-    
-        case $elasticsearch_version in
-        '7.6.2'|'6.5.4'|'6.4.2'|'5.6.8'|'2.4.6'|'2.4.4'|'1.7.6')
-            break;
-        ;;
-        *)
-            warp_message_info2 "Selected: $elasticsearch_version, the available versions are: 7.6.2, 6.5.4, 6.4.2, 5.6.8, 2.4.6, 2.4.4, 1.7.6"
-        ;;
-        esac        
+        elasticsearch_version=$( warp_question_ask_default "Choose a version of elasticsearch: $(warp_message_info ["${last_tag}"]) " "${last_tag}" )
+        if [[ "$elasticsearch_version" =~ ^($image_tags_switch)$ ]]; then
+            break
+        else
+            warp_message_info2 "Selected: $elasticsearch_version, $image_tags"
+        fi
     done
+
     warp_message_info2 "Selected version of elasticsearch: $elasticsearch_version, in the internal ports 9200, 9300 $(warp_message_bold 'elasticsearch:9200, elasticsearch:9300')"
     elasticsearch_memory=$( warp_question_ask_default "Set memory limit of elasticsearch: $(warp_message_info [512m]) " "512m" )
     warp_message_info2 "Selected memory limit of elasticsearch: $elasticsearch_memory"
