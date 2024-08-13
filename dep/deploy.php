4879ac040977db3ff55b8e282e9021c26e5df7b7
Leandro Ascaino <leandro.ascaino@gmail.com>
2024-08-13 19:41:44 -0300

    NO_TKT: Fixes for Jenkins deployment

--- dep/deploy.php
+++ dep/deploy.php
@@ -6,17 +6,21 @@ require 'recipe/common.php';
 set('default_timeout', 500);
 set('git_ssh_command', 'ssh');
 
+
+desc('Update code from git');
+task('warp:git-pull', function () {
+    run('git pull --rebase origin bestworlds');
+});
+
 desc('Create new release');
 task('warp:new-release', function () {
-    //run('bash release.sh');
-    run('ls -la');
+    run('bash release.sh');
 });
 
 desc('Execute deployment');
 task('deploy', [
+    'warp:git-pull',
     'warp:new-release',
 ]);
-
-after('deploy', 'success');
 
 import('hosts.yml');
